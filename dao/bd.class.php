018670769deb008598ec9f34f08788be369be381
JohanPE <johanpe050702@gmail.com>
2023-10-26 23:02:41 -0500

    Linea1 agregado

--- dao/bd.class.php
+++ dao/bd.class.php
@@ -5,7 +5,7 @@ class Bd{
     public function getConnectionMYSQL(){
         $servidor="localhost";
         $usuario="root";
-        $clave="09082002campos";
+        $clave="";
         $basedatos="bdmetro";
         $cnx=null;
         $cnx=new mysqli($servidor,$usuario,$clave,$basedatos);
